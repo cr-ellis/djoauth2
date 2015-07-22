cea0f45b687ecf54827ab3eeaa32111d7ae3d4dd
Colin Ellis <panamayacht@gmail.com>
2015-07-22 10:41:16 -0500

    don't fail if app tries multiple times to authenticate

--- djoauth2/authorization.py
+++ djoauth2/authorization.py
@@ -329,7 +329,7 @@ class AuthorizationCodeGenerator(object):
     when the user grants the Client's authorization request. The request is
     assumed to have successfully been vetted by the :py:meth:`validate` method.
     """
-    new_authorization_code = AuthorizationCode.objects.create(
+    new_authorization_code, _ = AuthorizationCode.objects.get_or_create(
         user=self.user,
         client=self.client,
         redirect_uri=(self.redirect_uri if self.request_redirect_uri else None)
